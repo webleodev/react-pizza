6b5534d9a81272149193f7a9e5140470821beb2b
Albert <62692427+webleodev@users.noreply.github.com>
2020-09-18 19:35:36 +0300

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,38 +1,5 @@
 import React from 'react'
 
-// class Categories extends React.Component {
-//     state = {
-//         activeItem: '',
-//     };
-
-//     onSelectItem = index => {
-//         this.setState({
-//             activeItem: index,
-//         });
-//     };
-
-//     render() {
-//         const { items } = this.props;
-
-//         return (
-//             <div className="categories">
-//                 <ul>
-//                     <li className="active">Все</li>
-
-//                     {items.map((name, index) => (
-//                         <li 
-//                             className={this.state.activeItem === index ? 'active' : ''}
-//                             onClick={() => this.onSelectItem(index)}
-//                             key={`${name}_${index}`}
-//                         >
-//                             {name}
-//                         </li>))};
-//                 </ul>
-//             </div >
-//         );
-//     }
-// }
-
 function Categories({ items }) {
     const [activeItem, setActiveItem] = React.useState(null);
 
