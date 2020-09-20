408a0aa44903788dd90c69d97a06a49248fc2fcb
ITDev-front <web.leo.develop@gmail.com>
2020-09-20 17:08:37 +0300

    Added DB, new component with funcs and axios req

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    })
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
